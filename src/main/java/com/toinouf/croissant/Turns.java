2402db47264dce11ef38bae8460029392ea13a11
Antoine Michaud <amichaud.prestataire@voyages-sncf.com>
2015-03-31 13:09:21 +0200

    Add next candidate

--- src/main/java/com/toinouf/croissant/Turns.java
+++ src/main/java/com/toinouf/croissant/Turns.java
@@ -5,8 +5,17 @@ import java.util.List;
 
 public class Turns {
     public Candidates history;
+    private Candidate nextCandidate;
 
     public List<String> history() {
         return new ArrayList<>();
+    }
+
+    public Candidate next() {
+        return nextCandidate;
+    }
+
+    public void addNext(Candidate candidate) {
+        nextCandidate = candidate;
     }
 }
--- src/test/java/com/toinouf/croissant/TurnsTest.java
+++ src/test/java/com/toinouf/croissant/TurnsTest.java
@@ -1,5 +1,6 @@
 package com.toinouf.croissant;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -7,11 +8,25 @@ import static org.assertj.core.api.Assertions.*;
 public class TurnsTest {
 
     @Test
-    public void history_should_be_empty_at_start() throws Exception {
+    public void history_should_be_empty_at_start_and_next_null() throws Exception {
         // Given
         Turns turns = new Turns();
 
         // When / Then
         assertThat(turns.history()).isEmpty();
+        assertThat(turns.next()).isNull();
+    }
+
+    @Test
+    public void should_add_user_to_next_and_leave_history_empty() throws Exception {
+        // Given
+        Candidate candidate = new Candidate("bla@bla.com");
+
+        Turns turns = new Turns();
+        turns.addNext(candidate);
+
+        // When / Then
+        assertThat(turns.history()).isEmpty();
+        assertThat(turns.next()).isEqualTo(candidate);
     }
 }
